2e5e3b0a98b077b25fb77a5639b572280d50377d
Sam Colson <samcolson4@gmail.com>
2023-10-15 07:35:51 +0100

    Inital rubocop clean-up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Rails generic helper
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Rails generic helper
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
